951aaebac6aa92d41809069ae2a7ef1870aa59ab
vanvibig <vanvibig@gmail.com>
2021-04-03 10:23:15 +0700

    init project with vuex4

---
+++ src/components/ProductList.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>Product List</h1>
+        <ul>
+            <li v-for="product in products" v-bind:key="product.id">
+                {{ product.title }} - {{ product.price }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import shop from '../api/shop'
+
+export default {
+    data() {
+        return {
+            products: []
+        }
+    },
+    created() {
+        shop.getProducts(products => {
+            this.products = products
+        })
+    }
+}
+</script>
+
+<style>
+
+</style>
